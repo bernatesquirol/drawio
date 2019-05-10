5b1ac0fef2587c12ee0272391e6f2729aee51d63
Unknown <bernatesquirol@gmail.com>
2019-05-10 08:17:08 +0200

    intent findRelated, not working + canvi findChildren

--- src/main/webapp/flowio/drawio-node.js
+++ src/main/webapp/flowio/drawio-node.js
@@ -65,22 +65,26 @@ const getProps = function(root){
   return values.$
 
 }
-const findChildren = function(root, obj_filter){
+const findChildren = function(root, filter_func){
   if (root==null) return null
   let props = getProps(root)
   if (props!=null){
-    if (_.reduce(obj_filter,(result, value, key) =>
-  (result && props[key]==value),true)) {
-    return root
-  }
+    console.log(filter_func(props))
+    if (filter_func==null | filter_func(props)) {
+      return root
+    }
   }
   let children = getChildren(root)
 
   if (children==null || children.length<1) return null
-  let return_value = _.flatMap(children,(child)=>findChildren(child,obj_filter)).filter((child)=>child!=null)
+  let return_value = _.flatMap(children,(child)=>findChildren(child,filter_func)).filter((child)=>child!=null)
   return return_value
 }
 
+const findChildrenValueFilter = function(root, obj_filter){
+  return findChildren(root, (props)=>_.reduce(obj_filter,(result, value, key) =>
+  (result && props[key]==value),true))
+}
 
 
 const parseString = function(stringToParse, args_parser={}){
@@ -200,6 +204,42 @@ const getDiagram = (array_obj, root_id)=>{
 
 }
 
+const findRelated=(xml_obj, id_related, already_seen={})=>{
+  if (already_seen[id_related]==true) return [null,already_seen]
+  let new_already_seen = {...already_seen}
+  let filter_related = (id_to_find)=>{
+    return (props)=>{
+      return props.parent==id_to_find || props.target==id_to_find || props.source==id_to_find
+    }
+  }
+  new_already_seen[id_related]=true
+  let sons_and_connected = findChildren(xml_obj, filter_related(id_related))
+  console.log(sons_and_connected)
+  let id_to_search = sons_and_connected.map((item)=>{
+    if(item.$.target==id_related){
+      return item.$.source //afegim edge i busquem l'altre punta
+    }else if(item.$.source==id_related){
+      return item.$.target //afegim edge i busquem l'altre punta
+    }
+    return item.$.id //afegim fill i busquem dins
+  })
+  let him = findChildrenValueFilter(xml_obj, {'id':id_related})  
+  //if xml_obj.$.parent
+  if (him.$.parent){
+    let parent = findChildrenValueFilter(xml_obj, {'id':him.$.parent})  
+    if(Object.keys(parent.$).includes('flowio_key') && parent.id){
+      id_to_search.push(parent.id)
+    }
+  }
+
+  let recursive_step = id_to_search.map((id_item)=>{
+    let result = findRelated(xml_obj, id_item, new_already_seen)
+    let list_of_all = result[0]
+    new_already_seen = result[1]
+    return list_of_all
+  }).flat()
+  return recursive_step, new_already_seen
+}
 
 module.exports={
   //encode: encode,
@@ -212,9 +252,12 @@ module.exports={
   getClearLabels:getClearLabels,
   modifySimpleBlock:modifySimpleBlock,
   findChildren:findChildren,
+  findChildrenValueFilter:findChildrenValueFilter,
   getDiagram:getDiagram,
   explicitChildrenToNot:explicitChildrenToNot,
-  getGeoSimpleBlock:getGeoSimpleBlock
+  getGeoSimpleBlock:getGeoSimpleBlock,
+  findRelated:findRelated,
+
 }
 
 /*
